ec77695a187aeebda14d857b6a389e3d6564952a
VAISHNAVRAVINDRAN <104183316+VAISHNAVRAVINDRAN@users.noreply.github.com>
2022-06-01 21:22:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-const ho_btn=document.querySelector(".home-btn");
-const ac_btn=document.querySelector(".account-btn");
-const co_btn=document.querySelector(".code-btn");
-const ab_btn=document.querySelector(".code-btn");
+
